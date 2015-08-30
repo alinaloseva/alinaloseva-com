da6722775a94009a3f52a98479bf6df87976ae4c
Igor Line <igor@line.ee>
2015-08-30 21:30:42 +0200

    imagesLoaded

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 $(function() {
-    $('.grid').isotope({
-        layoutMode: 'packery',
-        itemSelector: '.grid-item',
-        columnWidth: '.grid-sizer',
-        percentPosition: true
+    var $grid = $('.grid').imagesLoaded( function() {
+        $grid.isotope({
+            layoutMode: 'packery',
+            itemSelector: '.grid-item',
+            columnWidth: '.grid-sizer',
+            percentPosition: true
+        });
     });
 });
